319c62a1c190436ca93362ecac3522e9c6afe915
Vinicius Augutis <vinicius_augutis@hotmail.com>
2019-04-01 23:53:36 -0300

    Adding section to skills

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ function invertTheme() {
 window.onload = function () {
     getEducation();
     getExperience();
+    getSkills();
 };
 
 function getEducation() {
@@ -78,5 +79,33 @@ function updateExperience(data) {
     })
 
     experience.innerHTML = list.join('')
+}
+
+
+function getSkills() {
+    fetch('http://demo9495980.mockable.io/skills').then(response => {
+        return response.json()
+    })
+        .then(data => {
+            updateSkills(data)
+        });
+}
+
+function updateSkills(data) {
+    var skill = document.getElementById("skill-list")
+    var list = data.company.map((e) => {
+        var html =
+            `<div class="CV-timeline CV-experience-group">
+                    <h3 class="CV-timeline-heading">
+                        <span class="CV-timeline-heading-title">${e.nome}</span><small
+                            class="CV-timeline-heading-duration">${e.nivel}
+                           </small>
+                    </h3>
+                </div>`
+
+        return html
+    })
+
+    skill.innerHTML = list.join('')
 }
 
